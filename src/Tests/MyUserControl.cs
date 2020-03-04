6dc7b6697a2cb4b7143d676d2223f953037091ba
SimonCropp <simon.cropp@gmail.com>
2020-03-04 21:00:48 +1100

    .

---
+++ src/Tests/MyUserControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+#pragma warning disable 8618
+
+namespace WindowsFormsApp1
+{
+    public partial class MyUserControl : UserControl
+    {
+        private Button button1;
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(82, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            //
+            // MyForm
+            //
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.button1);
+            this.Name = "MyForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
