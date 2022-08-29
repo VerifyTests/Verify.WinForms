43f889f7d515b2364a61b8a3fce3b02b0a28f440
Simon Cropp <simon.cropp@gmail.com>
2022-08-29 14:19:43 +1000

    refs

---
+++ src/Tests/ModuleInit.cs
@@ -0,0 +1,17 @@
+﻿public static class ModuleInit
+{
+    #region enable
+
+    [ModuleInitializer]
+    public static void Init()
+    {
+        VerifyWinForms.Enable();
+
+        #endregion
+
+        VerifierSettings.UniqueForRuntime();
+        VerifyPhash.RegisterComparer("png", .99f);
+
+        VerifyDiffPlex.Initialize();
+    }
+}
